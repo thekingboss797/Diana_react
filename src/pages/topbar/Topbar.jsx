9480414867cbe23ed590be49dc10ca33e846824c
angelcruz797 <angelcruzcespedes797@gmail.com>
2024-10-20 22:15:26 -0400

    cambios en el topbar
    logo principal mas pequeño, quitandole margen al nav

--- src/pages/topbar/Topbar.jsx
+++ src/pages/topbar/Topbar.jsx
@@ -50,7 +50,7 @@ export const Topbar = () => {
   return (
     <header className="topbar h-10 max-md:h-20 w-full z-50 bg-neutral-900 flex flex-row flex-wrap items-center justify-between text-base text-white font-semibold fixed">
         {/* logo */}
-        <div className="item-topbar-logo w-1/12 pl-5 h-full items-center flex flex-row max-md:justify-start justify-stretch">
+        <div className="item-topbar-logo w-1/12 p-1 h-full items-center flex flex-row max-md:justify-start justify-stretch">
             <div>
                 <IconButton onClick={()=>handleDrawerToggle(true)} edge='start' sx={{color:'#ffffff', display: { xl: 'none' } }}><MenuIcon/></IconButton>
                 <Drawer 
@@ -86,7 +86,7 @@ export const Topbar = () => {
             <img className='logo h-3/4 fill cursor-pointer' src={RobloxIcon} alt="Roblox logo"/>
         </div>
         {/* nav */}
-        <Stack direction='row' className="item-topbar-nav w-4/12 h-full leading-10 items-stretch justify-center">
+        <Stack direction='row' className="item-topbar-nav w-4/12 h-full -ml-[70px] max-md:m-[0] leading-10 items-stretch justify-center">
             {['Charts','Marketplace','Create','Robux'].map((data)=>(
                 <Paper key={data}  sx={{borderRadius:0,backgroundColor:'transparent',color:'#fff'}} className='w-1/4 hover:border-b-2 cursor-pointer text-center'>
                     <a href='#' className=''>{data}</a>
@@ -97,7 +97,7 @@ export const Topbar = () => {
         <div className="item-topbar-util flex flex-row max-md:w-full w-6/12 h-full justify-items-end justify-end items-center">
             {/* search */}
             <div className="max-md:w-full w-3/5 h-full">
-                <div className='hidden max-md:flex max-md:flex-row-reverse max-md:items-center max-md:h-full'>
+                <div className='hidden max-md:flex max-md:flex-row-reverse max-md:items-center max-md:h-4/5'>
                     <SearchIcon />
                 </div>
                 <div className='relative visible py-2 w-full max-md:hidden'>
